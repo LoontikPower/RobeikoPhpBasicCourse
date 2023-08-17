cd2de2e93247b4cbadd7e3367fc66bc6fc3917f5
Serhii Robeiko <leisureloontik@gmail.com>
2023-08-17 09:46:23 +0300

    change if-else and require Surname-dz4.php

--- Surname-dz4.php
+++ Surname-dz4.php
@@ -7,22 +7,10 @@
 напис ( "Довжина вашого прізвища дорівнює {Довжина прізвища} і вона більша за {Константа при якій вона спрацювала}
 і менше ніж {наступна константа в керуючій конструкції (якщо це не else)}" )*/
 
-$last_name='Robeiko';
+$last_name='Robeikotttt';
 
 const SMALL_SURNAME=5;
 const MEDIUM_SURNAME=10;
 const LARGE_SURNAME=15;
 
 var_dump($last_name);
-echo "<br>";
-
-if((strlen($last_name)>SMALL_SURNAME) && (strlen($last_name)<MEDIUM_SURNAME) && (strlen($last_name)<LARGE_SURNAME) ){
-    echo $last_name.strlen($last_name)." Last name bigger than" . " SMALL_SURNAME=".SMALL_SURNAME;
-    echo "<br>";
-    echo "Small than MEDIUM_SURNAME= ".MEDIUM_SURNAME;
-    echo "<br>";
-    echo " and small than LARGE_SURNAME= ".LARGE_SURNAME;
-}
-else{
-    echo "ERROR";
-}
--- index.php
+++ index.php
@@ -1,4 +1,30 @@
 <?php
-phpinfo();
+require __DIR__. "/Surname-dz4.php";
+
+
+global $last_name;
+
+echo "<br>";
+if(strlen($last_name)<SMALL_SURNAME)
+    {
+        echo "Very small name";
+        echo "<br>";
+    }
+else if((strlen($last_name)>SMALL_SURNAME) && (strlen($last_name)<MEDIUM_SURNAME) ){
+
+    echo $last_name.strlen($last_name)." Last name bigger than" . " SMALL_SURNAME=".SMALL_SURNAME;
+    echo "<br>";
+    echo "and small than MEDIUM_SURNAME= ".MEDIUM_SURNAME;
+
+}
+else if ( (strlen($last_name)>MEDIUM_SURNAME)&&(strlen($last_name)<LARGE_SURNAME) ){
+
+    echo "  small than LARGE_SURNAME= ".LARGE_SURNAME;
+    echo "<br>";
+}
+else
+{
+    echo "  Bigger than LARGE_SURNAME= ".LARGE_SURNAME;
+}
 
 
